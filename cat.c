cb4d57199c1dab2f296055e087bc56793d7adb2a
root <root@uhfbmqwvtg.local>
2025-02-12 12:00:43 +0000

    simplify solution

--- cat.c
+++ cat.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char *get_text(char *file_path)
-{
-    FILE *file = fopen(file_path, "r");
+int main(int args_count, char ** args)
+{   
+    if(args_count > 1){
 
-    long int res = ftell(file);
-    char *str = (char *)malloc(sizeof(char) * res);
-    int i = 0;
-    while (!feof(file))
-    {
-        str[i] = getc(file);
-        i++;
+        FILE *file = fopen(args[1], "r");
+
+        while (!feof(file))
+        {
+            putc(getc(file),stdout);
+        }
+
+
+        fclose(file);
     }
-
-    fclose(file);
-    return str;
-}
-
-int main(int args_count, char *args[])
-{
-    if (args_count > 2)
-    {
-        char* text = get_text(args[1]);
-        printf(text);
-    }
+    return 0;
 }
